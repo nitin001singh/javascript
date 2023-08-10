0583e63456eba5d581162b86c6eef70f02458dfe
Nitin Singh <nitin001.singh@gmail.com>
2023-08-10 22:47:43 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,127 @@
+// console.log('Hi');
+
+// Variable & constants & comments
+
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// let b = 10;
+// b = 20;
+
+// console.log(b);
+
+// const c= 10;
+// console.log(c);
+
+// Data Types & Values
+
+// Numbers, String, Boolean, Null, Undefined, object, symbol
+
+// let points;
+// points = 13
+// console.log(points);
+
+// let a = "Hello";
+// let b = "World";
+// let c = "        Coding        "
+// console.log(a,b);
+// console.log(a+b);
+// console.log(`My string is: ${a} ${b}`);
+// console.log(a[2]);
+// console.log((a+b).toLowerCase());
+// console.log((a+b).toUpperCase());
+
+// console.log(a.indexOf('H'));
+
+// console.log(c);
+// console.log(c.trim());
+// console.log(c);
+
+// console.log(a.lastIndexOf('H'));
+
+// console.log(a.includes('H'));
+// console.log(a.includes('P'));
+
+// let d = "MY text goes here"
+
+// console.log(d.slice(0,5));
+
+// console.log(d.split(' '));
+
+// // Strings are immutable
+
+// let str = "Hello";
+// console.log(str);
+
+// str[0] = 'P'
+// console.log(str);
+
+// // Numbers
+
+// let num = 10;
+// console.log(typeof num);
+// console.log(0%5);
+// console.log(2**5);
+// console.log('1 + 2 is ', 1+2);
+
+// let a1 = 2
+// console.log(a1 == 2);
+// console.log(a1 === '2');
+
+// console.log(a1 != 2);
+// console.log(a1 !== '2');
+
+// let a3 = "54"
+// let a2 = Number(a3)
+
+// console.log(a3 , typeof a3);
+// console.log(a2, typeof a2);
+
+// console.log(String(5));
+// console.log(Boolean(-1));
+// console.log(Boolean(' '));
+
+// Array Methods
+
+// let arr = ['A','B','C','B']
+// let arr1 = ['E','F']
+// console.log(arr.join(','));
+
+// console.log(arr.indexOf('B'));
+// console.log(arr.concat(arr1));
+
+// console.log(arr.length);
+
+// push method
+
+// let pushedarr = arr.push('Nitin')
+// console.log(pushedarr);
+// console.log(arr);
+
+// // pop
+
+// console.log(arr.pop());
+// console.log(arr);
+// console.log('---');
+
+// let points = "23"
+// console.log(points === 23);
+
+// console.log(3%8);
+
+// console.log("5"+3);
+
+
+// console.log("5"-3);
+// console.log("5"*3);
+
+// loop
+// for,while,do-while
+
+// let a = 50;
+// if(true){
+//     a = 10;
+//     console.log(a);
+// }
+// console.log(a);
